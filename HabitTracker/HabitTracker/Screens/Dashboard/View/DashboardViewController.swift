4d990006a0b883f524888996ef25945bf4f1bfbb
busesahinbas <buse.sahinbas98@gmail.com>
2025-01-26 18:17:19 +0300

    Feat register cells on DashboardViewController

--- HabitTracker/HabitTracker/Screens/Dashboard/View/DashboardViewController.swift
+++ HabitTracker/HabitTracker/Screens/Dashboard/View/DashboardViewController.swift
@@ -8,23 +8,44 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController {
-
+final class DashboardViewController: UIViewController {
+    @IBOutlet private(set) weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        tableView.register(UINib(nibName: "UserTableViewCell", bundle: nil), forCellReuseIdentifier: "UserTableViewCell")
+        tableView.register(UINib(nibName: "ProgressTableViewCell", bundle: nil), forCellReuseIdentifier: "ProgressTableViewCell")
     }
-    */
+}
 
+extension DashboardViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserTableViewCell", for: indexPath) as? UserTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProgressTableViewCell", for: indexPath) as? ProgressTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+
+        default:
+            return UITableViewCell()
+        }
+    }
 }
